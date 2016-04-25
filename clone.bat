286a18f69b8c4412abdbbe90229b05fe1d1756ab
Matthew Beatty <beattyml1@gmail.com>
2016-04-25 18:22:22 -0400

    Update clone.bat

--- clone.bat
+++ clone.bat
@@ -1,3 +1,6 @@
-ECHO "Enter Repository Name"
-set /p repoName="RepoName"
+set repoName = %0
+if %repoName% == "" ( 
+  ECHO "Enter Repository Name"
+  set /p repoName="RepoName"
+  )
 git clone https://github.com/beattyml1/%repoName%.git src/%repoName%
