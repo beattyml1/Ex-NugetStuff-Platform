c70157236ebf9ba87bccfc1d138be479ed177e19
Matthew Beatty <beattyml1@gmail.com>
2016-04-25 20:02:52 -0400

    Create clone-basics.sh

---
+++ scripts/clone-basics.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git clone https://github.com/beattyml1/Ex-NugetStuff-ProjectTemplates.git ProjectTemplates
+git clone https://github.com/beattyml1/Ex-NugetStuff-TeamSettings.git TeamSettings
+git clone https://github.com/beattyml1/Ex-NugetStuff-DevSettings.git DevSettings
+git clone https://github.com/beattyml1/Ex-NugetStuff-BuildUtils.git src/BuildUtils
+git clone https://github.com/beattyml1/Ex-NugetStuff-ThirdPartyBins.git bin/ext
+gti clone https://github.com/beattyml1/Ex-NugetStuff-Common.git src/common
